87714d6c9ec617bff9c16168c6d6d8897ec1e267
KelpieLord <nympheraa@gmail.com>
2021-09-14 17:21:26 +0300

    Yard Greening task done

--- ProgrammingBasicsCSharp/FirstSteps/FirstStepsInCoding/YardGreening/Program.cs
+++ ProgrammingBasicsCSharp/FirstSteps/FirstStepsInCoding/YardGreening/Program.cs
@@ -6,7 +6,17 @@ namespace YardGreening
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double yardSize = double.Parse(Console.ReadLine());
+            double yardGreeningPrice = 7.61;
+            double discount = 0.18;
+
+            double priceCalc = yardSize * yardGreeningPrice;
+            double finalDiscount = discount * priceCalc;
+            double finalPrice = priceCalc - finalDiscount;
+
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {finalDiscount} lv.");
+
         }
     }
 }
