8be9740b4770881139f168c56c9b0de85ccc0d53
KelpieLord <nympheraa@gmail.com>
2021-09-15 16:37:06 +0300

    Started Exercises

---
+++ ProgrammingBasicsCSharp/FirstSteps/FirstStepsInCodingExercise/USDtoBGN/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double usdToBgnConversionRate = 1.79549;
+            double bgn = usd * usdToBgnConversionRate;
+
+            Console.WriteLine(bgn);
+
+        }
+    }
+}
