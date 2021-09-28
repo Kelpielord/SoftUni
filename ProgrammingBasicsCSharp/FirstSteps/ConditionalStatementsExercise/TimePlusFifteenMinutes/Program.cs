3d996233537a39531a541403ab776885a5368818
KelpieLord <nympheraa@gmail.com>
2021-09-28 12:41:41 +0300

    Finishing Conditionals Exercise

---
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsExercise/TimePlusFifteenMinutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TimePlusFifteenMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int fifteenMinutes = 15;
+            int totalMinutes = hours * 60 + minutes;
+            int hoursCalc = (totalMinutes + fifteenMinutes) / 60;
+            int minutesCalc = (totalMinutes + fifteenMinutes) % 60;
+
+            if (hoursCalc == 24)
+            {
+                hoursCalc = 0;
+            }
+            Console.WriteLine($"{hoursCalc}:{minutesCalc:D2}");
+
+        }
+    }
+}
---
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsExercise/ToyShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double travelPrice = double.Parse(Console.ReadLine());
+            int soldPuzzels = int.Parse(Console.ReadLine());
+            int soldDolls = int.Parse(Console.ReadLine());
+            int soldTeddybears = int.Parse(Console.ReadLine());
+            int soldMinions = int.Parse(Console.ReadLine());
+            int soldTrucks = int.Parse(Console.ReadLine());
+
+            double puzzelsMoney = soldPuzzels * 2.60;
+            double dollsMoney = soldDolls * 3;
+            double teddybearsMoney = soldTeddybears * 4.1;
+            double minionsMoney = soldMinions * 8.2;
+            double trucksMoney = soldTrucks * 2;
+
+            double total = puzzelsMoney + dollsMoney + minionsMoney + teddybearsMoney + trucksMoney;
+
+            int soldToys = soldPuzzels + soldDolls + soldTeddybears + soldTrucks + soldMinions;
+
+            if (soldToys >= 50)
+            {
+                total = total * 0.75;
+            }
+            total = total * 0.9;
+
+            if (total >= travelPrice)
+            {
+                double remaining = total - travelPrice;
+                Console.WriteLine($"Yes! {remaining:f2} lv left.");
+            }
+            else
+            {
+                double neededMoney = travelPrice - total;
+                Console.WriteLine($"Not enough money! {neededMoney:f2} lv needed.");
+            }
+        }
+    }
+}
