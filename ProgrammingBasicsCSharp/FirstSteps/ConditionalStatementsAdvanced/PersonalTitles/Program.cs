b810457d2d96e47f7e17b3896006b4fbbf966e49
KelpieLord <nympheraa@gmail.com>
2021-09-30 22:52:32 +0300

    Finished Tasks by 90%

--- ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvanced/PersonalTitles/Program.cs
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvanced/PersonalTitles/Program.cs
@@ -6,7 +6,32 @@ namespace PersonalTitles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "f")
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else if (gender == "m")
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+            
         }
     }
 }
