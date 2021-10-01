37af78b09f493d438d37ed8606884a0f8350fd94
KelpieLord <nympheraa@gmail.com>
2021-10-01 14:41:17 +0300

    Finished Nested ConditionalStatementsAdvanced

--- ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvanced/WorkingHours/Program.cs
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvanced/WorkingHours/Program.cs
@@ -6,7 +6,39 @@ namespace WorkingHours
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (day == "Sunday" || hour < 10 || hour > 18)
+            {
+                Console.WriteLine("closed");
+                return;
+            }
+
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("open");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("open");
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("open");
+                    break;
+                case "Thursday":
+                    Console.WriteLine("open");
+                    break;
+                case "Friday":
+                    Console.WriteLine("open");
+                    break;
+                case "Saturday":
+                    Console.WriteLine("open");
+                    break;
+                default:
+                    Console.WriteLine("Unknown");
+                    break;
+            }
         }
     }
 }
