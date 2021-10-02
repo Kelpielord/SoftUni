e4829b0458b87a2c00b2325c5c0faba1954815a5
KelpieLord <nympheraa@gmail.com>
2021-10-02 10:10:20 +0300

    Finished a few tasks

---
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvancedExercise/Journey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
