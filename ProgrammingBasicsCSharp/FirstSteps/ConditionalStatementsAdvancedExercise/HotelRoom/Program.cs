a7c1033fe2d2ca1fe619926836d1729116bb7b3c
Kelpielord <nympheraa@gmail.com>
2021-10-10 15:05:35 +0300

    Finished Conditional Statements Exercise

---
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvancedExercise/HotelRoom/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HotelRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int numberOfNights = int.Parse(Console.ReadLine());
+
+            double studio = 0;
+            double apartment = 0;
+            if (month == "May" || month == "October")
+            {
+                studio = 50;
+                apartment = 65;
+            }
+            else if (month == "June" || month == "September")
+            {
+                studio = 75.20;
+                apartment = 68.70;
+            }
+            else if (month == "July" || month == "August")
+            {
+                studio = 76;
+                apartment = 77;
+            }
+
+            double totalPriceForStudio = studio * numberOfNights;
+            double totalPriceForApartment = apartment * numberOfNights;
+            if ((numberOfNights > 7 && numberOfNights <= 14) && (month == "May" || month == "October")) // (numberOfNights > 7 && numberOfNights <= 14) instead (numberOfNights > 7 && numberOfNights < 14) & Order of operations - undefined priority of  parentheses
+            {
+                totalPriceForStudio *= 0.95;
+            }
+            else if (numberOfNights > 14 && (month == "May" || month == "October")) // Order of operations - undefined priority of parentheses
+            {
+                totalPriceForStudio *= 0.70;
+            }
+
+            if (numberOfNights > 14 && (month == "June" || month == "September"))// Order of operations - undefined priority of  parentheses
+            {
+                totalPriceForStudio *= 0.80;
+            }
+
+            if (numberOfNights > 14)
+            {
+                totalPriceForApartment *= 0.90;
+            }
+
+            Console.WriteLine($"Apartment: {totalPriceForApartment:f2} lv.");
+            Console.WriteLine($"Studio: {totalPriceForStudio:f2} lv.");
+        }
+    }
+}
---
+++ ProgrammingBasicsCSharp/FirstSteps/ConditionalStatementsAdvancedExercise/SkiTrip/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SkiTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine()) - 1;
+            string typeRoom = Console.ReadLine();
+            string revew = Console.ReadLine();
+            double roomPrice = 0;
+            double totalPrice = 0;
+            double discount = 0;
+
+            switch (typeRoom)
+            {
+                case "room for one person":
+                    roomPrice = 18;
+                    break;
+
+                case "apartment":
+                    roomPrice = 25;
+                    totalPrice = roomPrice * days; 
+
+                    if (days < 10)
+                    {
+                        discount = totalPrice * 0.30;
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        discount = totalPrice * 0.35;
+                    }
+                    else if (days > 15)
+                    {
+                        discount = totalPrice * 0.50;
+                    }
+                    break;
+
+                case "president apartment":
+                    roomPrice = 35;
+
+                    totalPrice = roomPrice * days; 
+
+                    if (days < 10)
+                    {
+                        discount = totalPrice * 0.10;
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        discount = totalPrice * 0.15;
+                    }
+                    else if (days > 15)
+                    {
+                        discount = totalPrice * 0.20;
+                    }
+                    break;
+            }
+            totalPrice = days * roomPrice - discount;
+            double finalSum = 0;
+
+            if (revew == "positive")
+            {
+                finalSum = totalPrice + (totalPrice * 0.25);
+            }
+            else
+            {
+                finalSum = totalPrice - (totalPrice * 0.10);
+            }
+            Console.WriteLine($"{finalSum:f2}");
+        }
+    }
+}
