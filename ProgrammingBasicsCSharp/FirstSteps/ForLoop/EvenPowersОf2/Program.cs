6f9b0cca1d742ab30fd92fd1d9fc2e44e1ecca30
Kelpielord <nympheraa@gmail.com>
2021-10-12 22:05:31 +0300

    Started For Loops Exercises

---
+++ ProgrammingBasicsCSharp/FirstSteps/ForLoop/EvenPowersОf2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EvenPowersОf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= nums; i+= 2)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
