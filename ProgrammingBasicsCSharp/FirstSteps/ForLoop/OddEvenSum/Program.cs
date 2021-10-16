49a409bc7222ec42f239a4c91d9f013d316bd0dd
Kelpielord <nympheraa@gmail.com>
2021-10-16 11:25:32 +0300

    Almost Finieshed With Foor Loops

---
+++ ProgrammingBasicsCSharp/FirstSteps/ForLoop/OddEvenSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
