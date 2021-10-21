78ee993bc62e573c1f5434221528e7aa7368584c
Kelpielord <nympheraa@gmail.com>
2021-10-21 17:25:51 +0300

    Commit

--- ProgrammingBasicsCSharp/FirstSteps/ForLoop/OddEvenSum/Program.cs
+++ ProgrammingBasicsCSharp/FirstSteps/ForLoop/OddEvenSum/Program.cs
@@ -6,7 +6,32 @@ namespace OddEvenSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                
+                if (i % 2 == 0)
+                {
+                    oddSum += num;
+                }
+                else
+                {
+                    evenSum += num;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
