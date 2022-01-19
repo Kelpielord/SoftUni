da8f76c407636f4bda0eee93ff0ea8c0eb4a2b70
Kelpielord <nympheraa@gmail.com>
2022-01-19 16:30:46 +0300

    Finished 90% of con_statements

---
+++ ProgrammingFundamentalsCSharp2022/Basic_Syntax_Conditional_Statements_Loops/06_StrongNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06_StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            double sum = 0;
+            double currentDigit = 0;
+            foreach (var c in n)
+            {
+                double factorialResult = 1;
+
+                var convertChar = char.GetNumericValue(c);
+                for (double i = convertChar; i >= 1; i--)
+                {
+                    factorialResult *= i;
+                }
+                sum += factorialResult;
+            }
+            
+            if (sum == int.Parse(n))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentalsCSharp2022/Basic_Syntax_Conditional_Statements_Loops/07_VendingMachine/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07_VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> validCoins = new List<double>() { 0.1, 0.2, 0.5, 1, 2};
+            Dictionary<string, double> products = new Dictionary<string, double>() 
+            {
+                { "Nuts", 2.0 },
+                { "Water", 0.7 },
+                { "Crisps", 1.5 },
+                { "Soda", 0.8 },
+                { "Coke", 1.0 },
+            };
+            double userCoins = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Start")
+                {
+                    break;
+                }
+                double coin = double.Parse(input);
+                if (validCoins.Contains(coin))
+                {
+                    userCoins += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine(); // "Nuts"
+                if (input == "End")
+                {
+                    break;
+                }
+
+                if (!products.ContainsKey(input))
+                {
+                    Console.WriteLine("Invalid product");
+                    continue;
+                }
+
+                double price = products[input];
+                
+                if (userCoins < price)
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                    continue;
+                }
+
+                userCoins -= price;
+                Console.WriteLine($"Purchased {input.ToLower()}");
+            }
+            Console.WriteLine($"Change: {userCoins:F2}");
+        }
+    }
+}
