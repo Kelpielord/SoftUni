9cecca65cfaff7ff0ed0ca75894a3867b60c25fc
Kelpielord <nympheraa@gmail.com>
2022-03-16 15:18:14 +0200

    Added 2 new finished tasks to objects and classes

---
+++ ProgrammingFundamentalsCSharp2022/Objects_and_Classes/00_Test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _00_Test
+{
+    internal class Program
+    {
+        class Song
+        {
+            public string TypeList { get; set; }
+
+            public string SongName { get; set; }
+
+            public string SongTime { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] tokens = Console.ReadLine().Split('_', StringSplitOptions.RemoveEmptyEntries);
+                string typeList = tokens[0];
+                string songName = tokens[1];
+                string songTime = tokens[2];
+
+                Song newSong = new Song()
+                {
+                    SongName = songName,
+                    TypeList = typeList,
+                    SongTime = songTime
+                };
+                songs.Add(newSong);
+            }
+
+            string songTypeListCommand = Console.ReadLine();
+            if (songTypeListCommand == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.SongName);
+                }
+            }
+            else
+            {
+                List<Song> orderedSongs = songs.FindAll(song => song.TypeList == songTypeListCommand);
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.SongName);
+                }
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentalsCSharp2022/Objects_and_Classes/07_OrderByAge/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_OrderByAge
+{
+    class Student
+    {
+        public Student(string name, string id, int age)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Student> students = new Dictionary<string, Student>();
+            while (true)
+            {
+                string[] tokens = Console.ReadLine().Split(' ').ToArray();
+                if (tokens[0] == "End")
+                {
+                    break;
+                }
+
+                string name = tokens[0];
+                string id = tokens[1];
+                int age = int.Parse(tokens[2]);
+                Student student = new Student(name, id, age);
+                students.Add(id, student);
+
+                ReplaceStudent(students, name, id, age);
+            }
+
+            PrintStudentsByAge(students);
+        }
+
+        private static void PrintStudentsByAge(Dictionary<string, Student> students)
+        {
+            foreach (var student in students.OrderBy(s => s.Value.Age))
+            {
+                Console.WriteLine($"{student.Value.Name} with ID: {student.Key} is {student.Value.Age} years old.");
+            }
+        }
+
+        private static void ReplaceStudent(Dictionary<string, Student> students, string name, string id, int age)
+        {
+            if (students.ContainsKey(id))
+            {
+                students[id].Name = name;
+                students[id].Age = age;
+            }
+        }
+    }
+}
